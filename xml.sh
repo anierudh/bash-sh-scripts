d3af2351d6f525e6a4981bf47bdc22a2c01737ad
anierudh <30382503+anierudh@users.noreply.github.com>
2018-09-15 23:38:40 +0530

    Add files via upload

--- xml.sh
+++ xml.sh
@@ -1,6 +1,9 @@
-first="AndroidManifest"
-last=".xml"
-num=0; for i in $(find . -maxdepth 2 -name '*AndroidManifest.xml'); do cp "$i" ./co/${first}${num}${last}; ((num++)); done
+num=0 
+for i in $(find . -maxdepth 2 -name '*AndroidManifest.xml')
+do
+cp "$i" ./benxml/$num.xml 
+((num++)) 
+done
 
 
 
